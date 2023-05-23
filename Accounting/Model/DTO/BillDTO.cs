65a12759116e5aec7efa5b3ad37e6bd1eb564182
DESKTOP-6BSG5L9\Admin <hungpham2020>
2023-05-23 23:56:54 +0700

    ok

--- Accounting/Model/DTO/BillDTO.cs
+++ Accounting/Model/DTO/BillDTO.cs
@@ -2,5 +2,22 @@
 {
     public class BillDTO
     {
+        public int Id { get; set; }
+
+        public int PersonId { get; set; }
+
+        public string PersonName { get; set; }
+
+        public int Type { get; set; }
+
+        public DateTime? ActiveDate { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public bool? IsPaid { get; set; }
+
+        public IEnumerable<MeatBillPrice> Items { get; set; }
     }
 }
--- Accounting/Repositories/Interfaces/IBillRepository.cs
+++ Accounting/Repositories/Interfaces/IBillRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace Accounting.Repositories.Interfaces
+﻿using Accounting.Common;
+using Accounting.Model.DTO;
+
+namespace Accounting.Repositories.Interfaces
 {
     public interface IBillRepository
     {
+        Pagination<BillDTO> GetAll(string keyword, DateTime? startDate, DateTime? endDate, int pageIndex, int pageSize);
+
+        bool AddBill(BillDTO res);
+
+        bool UpdateBill(BillDTO res);
+
+        bool DeleteBill(int id);
+
+        bool PayingBill(int id);
     }
 }
